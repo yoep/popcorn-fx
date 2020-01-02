a2cc2c49f6dacec147177d78c8f19adf8f2762a4
yoep <yoeri.peeters10@telenet.be>
2020-01-02 22:42:06 +0100

    Added additional debug logging to the VLC condition

--- video-vlc/src/main/java/com/github/yoep/video/vlc/condition/OnVlcCondition.java
+++ video-vlc/src/main/java/com/github/yoep/video/vlc/condition/OnVlcCondition.java
@@ -1,10 +1,12 @@
 package com.github.yoep.video.vlc.condition;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.ConfigurationCondition;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import uk.co.caprica.vlcj.factory.discovery.NativeDiscovery;
 
+@Slf4j
 class OnVlcCondition implements ConfigurationCondition {
     @Override
     public ConfigurationPhase getConfigurationPhase() {
@@ -14,6 +16,15 @@ class OnVlcCondition implements ConfigurationCondition {
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
         // check if a VLC installation can be found
-        return new NativeDiscovery().discover();
+        NativeDiscovery nativeDiscovery = new NativeDiscovery();
+        boolean nativeInstallationDiscovered = nativeDiscovery.discover();
+
+        if (nativeInstallationDiscovered) {
+            log.debug("Found native VLC installation at \"{}\"", nativeDiscovery.discoveredPath());
+        } else {
+            log.warn("VLC installation not found");
+        }
+
+        return nativeInstallationDiscovered;
     }
 }

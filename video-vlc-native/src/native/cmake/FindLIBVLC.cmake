e2aceaee3107a132cc56fbcb33bfba5080357ccd
yoep <yoeri.peeters10@telenet.be>
2020-11-08 11:04:48 +0100

    Fixed the find_library cmake command for VLC

--- video-vlc-native/src/native/cmake/FindLIBVLC.cmake
+++ video-vlc-native/src/native/cmake/FindLIBVLC.cmake
@@ -1,47 +1,37 @@
-FIND_PATH(LIBVLC_INCLUDE_DIR vlc/vlc.h
+FIND_PATH(
+        LIBVLC_INCLUDE_DIR vlc/vlc.h
         HINTS "$ENV{LIBVLC_INCLUDE_PATH}"
         PATHS
         "${CMAKE_CURRENT_SOURCE_DIR}/libvlc"
         "${CMAKE_CURRENT_SOURCE_DIR}/libvlc/vlc"
-        "$ENV{LIB_DIR}/include"
-        "$ENV{LIB_DIR}/include/vlc"
-        "/usr/include"
-        "/usr/include/vlc"
-        "/usr/local/include"
-        "/usr/local/include/vlc"
-        #mingw
-        c:/msys/local/include
-        )
-FIND_PATH(LIBVLC_INCLUDE_DIR PATHS "${CMAKE_INCLUDE_PATH}/vlc" NAMES vlc.h)
+)
 
-find_library(LIBVLC_LIBRARY NAMES vlc libvlc
+# fix the CMAKE_FIND_LIBRARY_SUFFIXES for windows as the list is incorrect
+# the original list contains ".ddl.a",".a",".lib"
+if (WIN32)
+    SET(CMAKE_FIND_LIBRARY_SUFFIXES ".dll" ".a" ".lib")
+endif ()
+
+FIND_LIBRARY(
+        LIBVLC_LIBRARY
+        NAMES vlc libvlc
+        HINTS "$ENV{LIBVLC_LIBRARY_PATH}"
         PATHS
-        "$ENV{LIBVLC_LIBRARY_PATH}"
-        "$ENV{LIB_DIR}/lib"
-        #Mac OS
-        "${CMAKE_CURRENT_SOURCE_DIR}/contribs/lib"
-        "${CMAKE_CURRENT_SOURCE_DIR}/contribs/plugins"
-        "/Applications/VLC.app/Contents/MacOS/lib"
-        "/Applications/VLC.app/Contents/MacOS/plugins"
-        #mingw
-        c:/msys/local/lib
-        "c:/Program Files/VideoLAN/VLC/"
-        )
-FIND_LIBRARY(LIBVLC_LIBRARY NAMES vlc libvlc)
-FIND_LIBRARY(LIBVLCCORE_LIBRARY NAMES vlccore libvlccore
+        "${PROJECT_SOURCE_DIR}/assets/linux/vlc"
+        "${PROJECT_SOURCE_DIR}/assets/linux-arm/vlc"
+        "${PROJECT_SOURCE_DIR}/assets/maxosx/vlc"
+        "${PROJECT_SOURCE_DIR}/assets/windows/vlc"
+)
+FIND_LIBRARY(
+        LIBVLCCORE_LIBRARY
+        NAMES vlccore libvlccore
+        HINTS "$ENV{LIBVLC_LIBRARY_PATH}"
         PATHS
-        "$ENV{LIBVLC_LIBRARY_PATH}"
-        "$ENV{LIB_DIR}/lib"
-        #Mac OS
-        "${CMAKE_CURRENT_SOURCE_DIR}/contribs/lib"
-        "${CMAKE_CURRENT_SOURCE_DIR}/contribs/plugins"
-        "/Applications/VLC.app/Contents/MacOS/lib"
-        "/Applications/VLC.app/Contents/MacOS/plugins"
-        #mingw
-        c:/msys/local/lib
-        "c:/Program Files/VideoLAN/VLC/"
-        )
-FIND_LIBRARY(LIBVLCCORE_LIBRARY NAMES vlccore libvlccore)
+        "${PROJECT_SOURCE_DIR}/assets/linux/vlc"
+        "${PROJECT_SOURCE_DIR}/assets/linux-arm/vlc"
+        "${PROJECT_SOURCE_DIR}/assets/maxosx/vlc"
+        "${PROJECT_SOURCE_DIR}/assets/windows/vlc"
+)
 
 IF (LIBVLC_INCLUDE_DIR AND LIBVLC_LIBRARY AND LIBVLCCORE_LIBRARY)
     SET(LIBVLC_FOUND TRUE)
@@ -55,7 +45,6 @@ if (LIBVLC_FOUND)
     endif (NOT LIBVLC_FIND_QUIETLY)
 else (LIBVLC_FOUND)
     if (LIBVLC_FIND_REQUIRED)
-        message(STATUS "ENVIRONMENT LIB_DIR=$ENV{LIB_DIR}")
         message(STATUS "ENVIRONMENT LIBVLC_INCLUDE_PATH=$ENV{LIBVLC_INCLUDE_PATH}")
         message(STATUS "ENVIRONMENT LIBVLC_LIBRARY_PATH=$ENV{LIBVLC_LIBRARY_PATH}")
         message(STATUS "LibVLC include-dir path: ${LIBVLC_INCLUDE_DIR}")

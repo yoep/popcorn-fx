25ae3345b23c8804454914b32476f0f9ffe41287
yoep <yoeri.peeters10@telenet.be>
2023-02-27 19:14:05 +0100

    #128: Removed spring components from video-vlc

--- application/src/main/java/com/github/yoep/popcorn/ui/config/VideoConfig.java
+++ application/src/main/java/com/github/yoep/popcorn/ui/config/VideoConfig.java
@@ -3,6 +3,8 @@ package com.github.yoep.popcorn.ui.config;
 import com.github.yoep.popcorn.backend.FxLib;
 import com.github.yoep.popcorn.backend.PopcornFx;
 import com.github.yoep.popcorn.backend.adapters.video.VideoPlayback;
+import com.github.yoep.video.vlc.VideoPlayerVlc;
+import com.github.yoep.video.vlc.VideoPlayerVlcError;
 import com.github.yoep.video.youtube.VideoPlayerYoutube;
 import com.github.yoep.video.youtube.conditions.OnWebkitSupportedCondition;
 import com.github.yoep.video.youtube.conditions.OnYoutubeVideoEnabled;
@@ -11,6 +13,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
+import uk.co.caprica.vlcj.factory.discovery.NativeDiscovery;
 
 @Slf4j
 @Configuration
@@ -24,5 +28,25 @@ public class VideoConfig {
         }
 
         return null;
+    }
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE + 10)
+    public VideoPlayback vlcVideoPlayer(NativeDiscovery nativeDiscovery) {
+        if (nativeDiscovery.discover()) {
+            log.debug("Discovered VLC library at {}", nativeDiscovery.discoveredPath());
+            return createVideoPlayerVlcInstance(nativeDiscovery);
+        } else {
+            log.warn("Failed to discover VLC library");
+            return new VideoPlayerVlcError();
+        }
+    }
+
+    private static VideoPlayerVlc createVideoPlayerVlcInstance(NativeDiscovery nativeDiscovery) {
+        log.trace("Creating VLC media player factory instance");
+        var mediaPlayerFactory = new MediaPlayerFactory(nativeDiscovery);
+
+        log.info("Using VLC player for video playbacks");
+        return new VideoPlayerVlc(mediaPlayerFactory);
     }
 }

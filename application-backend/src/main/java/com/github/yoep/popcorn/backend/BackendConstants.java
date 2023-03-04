b20ff209f05825bac204d2fe93b70733748bccad
yoep <yoeri.peeters10@telenet.be>
2023-03-04 21:12:03 +0100

    #136: Moved header view

--- application-backend/src/main/java/com/github/yoep/popcorn/backend/BackendConstants.java
+++ application-backend/src/main/java/com/github/yoep/popcorn/backend/BackendConstants.java
@@ -6,5 +6,4 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class BackendConstants {
     public static final String POPCORN_HOME_DIRECTORY = ".popcorn-time";
-    public static final String POPCORN_HOME_PROPERTY = ".popcorn-time";
 }
